333622826c0582beb62211e1e3a6538954c1e6bc
ILdar Shamiev <shamief@yandex.ru>
2019-04-15 18:27:14 +0300

    some fix

--- jenkins_build_identidock.sh
+++ jenkins_build_identidock.sh
@@ -10,8 +10,7 @@ sudo docker-compose $COMPOSE_ARGS up -d
 sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock ERR=$?
 # Выполнение тестирования системы в целом, если модульное тестирование завершилось успешно
 if [ $ERR -eq 0 ]; then
-        IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}}
-        jenkins_identidock_1)
+        IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
         CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
     if [ $CODE -ne 200 ]; then
         echo "Site returned " $CODE
