7dacbfb9a340973a507bfce42eeb31a77b677f39
ILdar Shamiev <shamief@yandex.ru>
2019-04-16 16:04:52 +0300

    Push to local registry

--- jenkins_build_identidock.sh
+++ jenkins_build_identidock.sh
@@ -11,6 +11,14 @@ sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
 ERR=$?
 # Выполнение тестирования системы в целом, если модульное тестирование завершилось успешно
 if [ $ERR -eq 0 ]; then
+        echo "Test passed - Tegging"
+        HASH=$(git rev-parse --short HEAD)
+        sudo docker tag -f jenkins_identidock localregistry:5000/identidock:$HASH
+        sudo docker tag -f jenkins_identidock localregistry:5000/identidock:newest
+        echo "Pushing"
+        sudo docker push localregistry:5000/identidock:$HASH
+        sudo docker push localregistry:5000/identidock:newest
+        
         IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
         CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
     if [ $CODE -ne 200 ]; then
